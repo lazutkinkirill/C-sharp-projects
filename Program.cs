a7286d7517fa526b549ad8cfbddfdc3085b78343
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-20 17:54:42 +0300

    add seminar project

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+//int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("write number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("квадрат " + a + " = " + (a * a));
+
